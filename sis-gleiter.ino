fd6b8948506f86178028894593ee7372835b0ad9
Alex "Memecian" Komyakov <ememecian@gmail.com>
2021-05-14 17:19:03 +0200

    update sis-gleiter.ino
    DEBUG :
    - added variable tracking in the serial monitor
    
    Other:
    - fixed servo issue
    - fixed timeout message spam
    - fixed display rotation

--- sis-gleiter.ino
+++ sis-gleiter.ino
@@ -19,8 +19,8 @@
 #define SERVO 	2
 
 // etc
-#define FLIGHT_TIME (115 * 60000) 	// Flight time in minutes
-#define SERVO_TIME 	(20 * 1000) 	// Servo rotation time in seconds TODO: add actual rotation time
+#define FLIGHT_TIME (115 * 60000UL) 	// Flight time in minutes
+#define SERVO_TIME 	(20 * 1000)	 		// Servo rotation time in seconds TODO: add actual rotation time
 
 #define DEBUG 1 // Comment this line to disable debug features
 
@@ -33,6 +33,10 @@ unsigned long current = 0;
 Adafruit_SSD1306 display(W, H, &Wire, RST);
 Servo servo;
 
+#ifdef DEBUG
+	char debugOut[128] = {0};
+#endif
+
 void setup(){
 
 #ifdef DEBUG
@@ -41,7 +45,7 @@ void setup(){
 #endif 	
     // setup here
 	display.begin(SSD1306_SWITCHCAPVCC, ADDR);
-  	display.setRotation(3); 
+  	display.setRotation(2); 
 	display.clearDisplay();
   	display.setTextColor(SSD1306_WHITE);
 	display.setCursor(0,0); 
@@ -66,17 +70,20 @@ void setup(){
 void loop(){
   	// loop here
 #ifdef DEBUG
+	remainOut = formatTime(remaining);
+	sprintf(debugOut, "started = %u\n released = %u\n remaining = %lu\n, remainOut = %s\n, current = %lu", started, released, remaining, remainOut, current);
     Serial.println("Loop!");
+	Serial.println(debugOut);
 #endif
     /* winding sequence 
 	 * just a loop to affix the glider to the balloon */
-	if (digitalRead(SERVO)){
-       servo.write(90);// if the button isn't pressed, stop the servo. 
+	if (!digitalRead(SERVO)){
+    	servo.write(90);// if the button isn't pressed, stop the servo. 
     }
 	else { 
-       servo.write(0); 		
+    	servo.write(0); 		
 #ifdef DEBUG
-      Serial.println("---Servo winding---");
+    Serial.println("---Servo winding---");
 #endif 
     }
 
@@ -86,12 +93,13 @@ void loop(){
 
 	if (!started && !digitalRead(START)) {
 		clearRect(0,0, 128, 9);
+		display.setCursor(0,0);
 		display.print("You sure?");
         display.display();
 #ifdef DEBUG
         Serial.println("--Start dialogue activated");
 #endif
-		delay(500);
+		delay(2000);
 		current = millis() + 10000;
 		// ten second timeout 
 		while (millis < current) {	
@@ -100,12 +108,12 @@ void loop(){
 				display.setCursor(0,0);
 				display.print("Time until release");
 				started = true;
-				display.setTextSize(3);
+				display.setTextSize(2);
 				break;
 			}
-#ifdef DEBUG
-            Serial.println("--Timed out");
-#endif
+		clearRect(0,0, 128, 9);
+		display.setCursor(0,0);
+		display.print("Ready to launch");		
 		}
 	}
 
@@ -114,8 +122,11 @@ void loop(){
 
 	while (started) {
 #ifdef DEBUG
-        Serial.println("Flight loop!");
+		Serial.println("Filght loop!");
+		sprintf(debugOut, "started = %u\n released = %u\nremaining = %lu\nremainOut = %s\ncurrent = %lu", started, released, remaining, formatTime(remaining), current);
+    	Serial.println(debugOut);
 #endif
+		remaining = FLIGHT_TIME;
        	clearRect(0, 9, 128, 32);
   		display.setCursor(0,8);
   		display.print(formatTime(remaining));
@@ -133,7 +144,7 @@ void loop(){
   
 	/* release sequence 
 	 * release the glider, print a song quote. */
-	
+
 	if (released) {
 #ifdef DEBUG
         Serial.println("Releasing glider");
@@ -155,11 +166,12 @@ void clearRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
 	display.drawRect(x, y, w, h, BLACK);
 	display.fillRect(x, y, w, h, BLACK);
 }
-char* formatTime(int time) {
+
+char* formatTime(long time) {
 	char* out = (char *) malloc(sizeof(char) * 21); // stops any complaints from the compiler.
-	int hh, mm, ss;
+	long hh, mm, ss;
 	time = time/60000;
-	hh = (int) floor(time / 60);
+	hh = (long) floor(time / 60);
 	mm = time % 60;
 	ss = 0;
 	sprintf(out, "%02.2hd:%02.2hd:%02.2hd", hh, mm ,ss);	
