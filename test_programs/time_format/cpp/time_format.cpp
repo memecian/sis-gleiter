56b30286f0c5a820a154450e2cdfa5c06ebb3b93
Alex "Memecian" Komyakov <ememecian@gmail.com>
2021-05-19 22:38:46 +0200

    moved time_format.cpp to separate dir

--- test_programs/time_format/cpp/time_format.cpp
+++ test_programs/time_format/cpp/time_format.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <cmath>
+#include <time.h>
 
 using namespace std;
 
 long random_time;
 
+char* formatTime(long time);
+
 int main(void){
+	srand(time(NULL));
+	cout << 0xFFFFFF << endl;
     while (1) {
-        random_time = rand() % 10000;
-        cout << formatTime(random_time) << "\n";
+        random_time = (rand() % 360000) * 1000;
+        cout << random_time <<"\n" << formatTime(random_time) << "\n";
+		cin.get();
     }
 }
 
 char* formatTime(long time) {
-	char out[20] ;
+	char *out;
 	long hh, mm, ss;
 	time = floor(time/1000); // convert to seconds from milliseconds
 	mm = time / 60;
 	ss = time % 60;
 	hh = mm / 60;	
 	mm = mm % 60;
-    printf("\n\n--%d:%d:%d--\n\n", hh, mm, ss);
+    printf("--%d:%d:%d--", hh, mm, ss);
 	sprintf(out, "%d:%d:%d", hh, mm ,ss);
 	return out;
 }
