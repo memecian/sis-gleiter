030c7967baed5e15780c2b40b71f372e1c810549
Alex "Memecian" Komyakov <ememecian@gmail.com>
2021-05-19 16:27:22 +0200

    updated programs

--- sis-gleiter.ino
+++ sis-gleiter.ino
@@ -21,18 +21,25 @@
 // etc
 #define FLIGHT_TIME (115 * 60000UL) 	// Flight time in minutes
 #define SERVO_TIME 	(20 * 1000)	 		// Servo rotation time in seconds TODO: add actual rotation time
+#define DEBUG
 
 /* global vars */
-bool started, released = false;
+bool started, dialogue, released = false;
 unsigned long remaining = FLIGHT_TIME;
-char* remainOut = (char*) malloc(sizeof(char) * 21); // reserve 8 chars for formatted time output. 21 bytes reserved, so the compiler shuts up
 unsigned long current = 0;
+
+/* global constants */
+const char ready[] = "Ready to launch";
 
 Adafruit_SSD1306 display(W, H, &Wire, RST);
 Servo servo;
 
 void setup() {
     // setup here
+#ifdef DEBUG
+	Serial.begin(9600);
+#endif // DEBUG
+
 	display.begin(SSD1306_SWITCHCAPVCC, ADDR);
   	display.setRotation(2); 
 	display.clearDisplay();
@@ -41,17 +48,59 @@ void setup() {
   	display.setTextSize(0);
 
     servo.attach(SERVO);
+
 	pinMode(START, INPUT_PULLUP);	// ATTENTION!
-	pinMode(WINDUP, INPUT_PULLUP);	// TO ACTIVATE BUTTONS, PINS NEED TO BE LOW! 
-    display.print(F("Ready to launch."));
+	pinMode(WINDUP, INPUT_PULLUP);	// TO ACTIVATE BUTTONS, PINS NEED TO BE PULLED LOW!  
+
+	display.print(ready);
     display.display();
 }
 
 void loop(){
+	
+	if (!digitalRead(START)) {
+#ifdef DEBUG
+	Serial.println("START pressed");
+#endif // DEBUG
+		dialogue = true;
+		clearRect(0, 0, 128, 9);
+		display.setCursor(0, 0);
+		display.print("Are you sure?");
+		display.display();
+		delay(500);
+		while (dialogue) {
+			if(!digitalRead(START)) {
 
+#ifdef DEBUG
+				Serial.println("START PRESSED AGAIN");
+#endif // DEBUG
+				display.setTextSize(2);
+				current = millis();	
+				while (remaining > 0) {
+					clearRect(0,0,128,32);
+					display.setCursor(0,0);
+					remaining = FLIGHT_TIME - (millis() - current);
+#ifdef DEBUG
+					Serial.println(formatTime(remaining));
+#endif // DEBUG			
+					display.print(formatTime(remaining));
+					display.display();
+				}
+			}
+
+			if (!digitalRead(WINDUP)) {
+#ifdef DEBUG
+				Serial.println("left dialogue");
+#endif // DEBUG			
+				dialogue = false;
+				clearRect(0, 0, 128, 9);
+				display.setCursor(0,0);
+				display.print(ready);
+				display.display();
+			}
+		} 
 	}
-
-
+} // loop
 
 /* function declarations */
 
@@ -60,18 +109,17 @@ void clearRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
 	display.fillRect(x, y, w, h, BLACK);
 }
 
-
 /* formatTime(time in ms)
  * returns remaining time in hh:mm:ss format */
 
 char* formatTime(long time) {
-	char* out = (char *) malloc(sizeof(char) * 21); // stops any complaints from the compiler.
+	char out[10] ;
 	long hh, mm, ss;
-	time = time/1000; // convert to seconds from milliseconds
+	time = floor(time/1000); // convert to seconds from milliseconds
 	mm = time / 60;
 	ss = time % 60;
 	hh = mm / 60;	
 	mm = mm % 60;
-	sprintf(out, "%02.2hd:%02.2hd:%02.2hd", hh, mm ,ss);	
+	sprintf(out, "%d:%d:%d", hh, mm ,ss);
 	return out;
 }
--- test_programs/button_input/button_input.ino
+++ test_programs/button_input/button_input.ino
@@ -1,9 +1,9 @@
 /* defines */
-#define BTN0 13
+#define BTN0 11
 #define BTN1 12
 
 /* global variables */
-char *out, *out_buf = (char*) malloc(sizeof(char) * 16);
+char out[8], out_buf[8];
 
 void setup() {
     pinMode(BTN0, INPUT_PULLUP);
@@ -13,6 +13,6 @@ void setup() {
 
 void loop() {
     sprintf(out, "%x|\t%x", digitalRead(BTN0), digitalRead(BTN1)); // zustaende der knoepfe werden hier gespeichert
-    if (strcmp(out, out_buf)) Serial.println(out); // sende nur, wenn sich die zustaende aendern
+    if (strcmp(out, out_buf)) Serial.println(out); // sende nur, wenn sich die zustaende aendern 
     strcpy(out_buf, out); // puffer update
 }
