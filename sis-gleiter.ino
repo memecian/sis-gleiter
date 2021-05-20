1f4ff78c5b73625b5443c61904f435a356a37ee0
Alex "Memecian" Komyakov <ememecian@gmail.com>
2021-05-20 09:19:33 +0200

    fixed malloc issues

--- sis-gleiter.ino
+++ sis-gleiter.ino
@@ -21,26 +21,32 @@
 // etc
 #define FLIGHT_TIME (115 * 60000UL) 	// Flight time in minutes
 #define SERVO_TIME 	(20 * 1000)	 		// Servo rotation time in seconds TODO: add actual rotation time
-#define DEBUG
+
+#define DEBUG 1 // Comment this line to disable debug features
 
 /* global vars */
 bool started, dialogue, released = false;
-char remain_out[20] = {0};
 unsigned long remaining = FLIGHT_TIME;
+char *remainOut = (char*) malloc(sizeof(char) * 21); // reserve 8 chars for formatted time output. 21 bytes reserved, so the compiler shuts up
+const char *ready = "Ready to launch.";
 unsigned long current = 0;
-
-/* global constants */
-const char ready[] = "Ready to launch";
 
 Adafruit_SSD1306 display(W, H, &Wire, RST);
 Servo servo;
 
-void setup() {
-    // setup here
 #ifdef DEBUG
-	Serial.begin(9600);
-#endif // DEBUG
+#define INTERVAL 1000 // interval in ms for debug messages
+	char debugOut[128] = {0};
+	long slu = millis() + INTERVAL; //SinceLastUpdate
+#endif
 
+void setup() {
+
+#ifdef DEBUG
+    Serial.begin(9600);
+    Serial.println("Starting display setup");
+#endif 	
+    // setup here
 	display.begin(SSD1306_SWITCHCAPVCC, ADDR);
   	display.setRotation(2); 
 	display.clearDisplay();
@@ -48,61 +54,72 @@ void setup() {
 	display.setCursor(0,0); 
   	display.setTextSize(0);
 
+#ifdef DEBUG
+    Serial.println("Display setup complete.");
+#endif
     servo.attach(SERVO);
-
 	pinMode(START, INPUT_PULLUP);	// ATTENTION!
-	pinMode(WINDUP, INPUT_PULLUP);	// TO ACTIVATE BUTTONS, PINS NEED TO BE PULLED LOW!  
-
-	display.print(ready);
+	pinMode(WINDUP, INPUT_PULLUP);	// TO ACTIVATE BUTTONS, PINS NEED TO BE LOW! 
+    display.print(ready);
     display.display();
+#ifdef DEBUG
+    Serial.println("Entering loop");
+#endif
 }
 
 void loop(){
-	
-	if (!digitalRead(START)) {
+  	// loop here
 #ifdef DEBUG
-	Serial.println("START pressed");
-#endif // DEBUG
-		dialogue = true;
-		clearRect(0, 0, 128, 9);
-		display.setCursor(0, 0);
-		display.print("Are you sure?");
-		display.display();
-		delay(500);
-		while (dialogue) {
-			if(!digitalRead(START)) {
+	Serial.println("Loop!");
+	if (slu <= millis()) debug_variables();
+#endif
+    /* winding sequence 
+	 * just a loop to affix the glider to the balloon */
+	if (!digitalRead(SERVO)){
+    	servo.write(90);// if the button isn't pressed, stop the servo. 
+    }
+	else { 
+    	servo.write(0); 		
+#ifdef DEBUG
+    Serial.println("---Servo winding---");
+#endif 
+    }
 
-#ifdef DEBUG
-				Serial.println("START PRESSED AGAIN");
-#endif // DEBUG
-				display.setTextSize(2);
-				current = millis();	
-				while (remaining > 0) {
-					clearRect(0,0,128,32);
-					display.setCursor(0,0);
-					remaining = FLIGHT_TIME - (millis() - current);
-					remain_out = formatTime(remaining);
-#ifdef DEBUG
-					Serial.println(formatTime(remain_out));
-#endif // DEBUG			
-					display.print(formatTime(remain_out));
-					display.display();
-				}
-			}
+	if (!started && !digitalRead(START)) startSequence();
 
-			if (!digitalRead(WINDUP)) {
+	/* main loop 
+	 * the loop that will be working most of the flight */
+
+	if (started) {
 #ifdef DEBUG
-				Serial.println("left dialogue");
-#endif // DEBUG			
-				dialogue = false;
-				clearRect(0, 0, 128, 9);
-				display.setCursor(0,0);
-				display.print(ready);
-				display.display();
+	debug_variables();
+#endif
+		remaining = FLIGHT_TIME;
+		clearRect(0, 0, 128, 32);
+		display.print("TIME UNTIL RELEASE");
+		while (started) {
+			clearRect(0, 9, 128, 32);
+	  		display.setCursor(0,10);
+			display.setTextSize(2);
+			formatTime(remaining);
+	  		display.print(remainOut);
+	        display.display(); 
+	 		delay(1000);
+	  		if (remaining < 0) remaining = remaining - 1000;
+			if (remaining == 0) {
+				started = false; 
+				released = true;
 			}
-		} 
+		}
+  	}
+  
+	/* release sequence 
+	 * release the glider, print a song quote. */
+
+	if (released) {
+		releaseSequence();
 	}
-} // loop
+}
 
 /* function declarations */
 
@@ -111,17 +128,76 @@ void clearRect(uint16_t x, uint16_t y, uint16_t w, uint16_t h) {
 	display.fillRect(x, y, w, h, BLACK);
 }
 
+/* startSequence()
+ * confirm launch by having the user press
+ * the START button twice				*/
+
+void startSequence(void) {
+	clearRect(0, 0, 128, 9);
+	display.setCursor(0,0);
+	display.print("Confirm launch");
+    display.display();
+	dialogue = true;
+#ifdef DEBUG
+    Serial.println("--Start dialogue activated");
+#endif
+	delay(500);
+	while (dialogue) {
+	if (!digitalRead(WINDUP)) {
+			clearRect(0, 0, 128, 9);
+			display.setCursor(0, 0);
+			display.print(ready);
+			display.display();
+#ifdef DEBUG
+			Serial.println("--Left start dialogue");
+#endif // DEBUG
+			break;
+		}
+		if (!digitalRead(START)) {
+			started = true;
+#ifdef DEBUG
+			Serial.println("--Started countdown.");
+#endif // DEBUG
+		break;
+		}
+	}
+}
+
 /* formatTime(time in ms)
  * returns remaining time in hh:mm:ss format */
 
-char* formatTime(long time) {
-	char out[20] ;
+void formatTime(long time) {
 	long hh, mm, ss;
-	time = floor(time/1000); // convert to seconds from milliseconds
+	time = time/1000; // convert to seconds from milliseconds
 	mm = time / 60;
 	ss = time % 60;
 	hh = mm / 60;	
 	mm = mm % 60;
-	sprintf(out, "%d:%d:%d", hh, mm ,ss);
-	return out;
+	sprintf(remainOut, "%02.2hd:%02.2hd:%02.2hd", hh, mm ,ss);	
 }
+
+void releaseSequence() {
+#ifdef DEBUG
+    Serial.println("Releasing glider");
+#endif
+    display.setCursor(0,0);
+	clearRect(0,0,128,32);
+	display.print(F("es gibt nichts\nwas mich haelt\nau revoir"));
+	display.display();
+    current = millis() + SERVO_TIME;
+	while (millis() < current)servo.write(180); 		// unwind the nuts
+		servo.write(90);							// stop rotation
+		while (1) asm volatile("nop"); 				// idle
+}		
+
+#ifdef DEBUG
+/* debug_variables()
+ * prints out a multiline string with all system vaiables at INTERVAL intervals */
+void debug_variables() {
+	formatTime(remaining);
+	Serial.println(remainOut);
+	sprintf(debugOut, "started = %u\nreleased = %u\nremaining = %lu\nremainOut = %s\ncurrent = %lu", started, released, remaining, remainOut, current);
+	Serial.println(debugOut);
+	slu = millis() + INTERVAL;
+}
+#endif // DEBUG
